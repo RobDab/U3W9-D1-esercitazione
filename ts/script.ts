c2b957d4e0446ecb6a3cd52c4d1271d1c978f403
RobDab <roberto.dabb@gmail.com>
2022-10-24 17:37:41 +0200

    fix bugs in output and evaluation of player errors

--- ts/script.ts
+++ ts/script.ts
@@ -16,25 +16,33 @@ class player {
     
     error(value:number) {
         let error:number = Math.abs(this.choice - value);
-        return error
+        return error;
     }
 }
 
 function startGame(){
 let p1 = new player('Rick', random())
 let p2 = new player('Morty', random())
-// console.log(p1.name, p1.choice, p1.error(50))
+
 
 let extracted:number = random()
 
 if(p1.choice == extracted){
-    console.log(`******\n ${p1.name} ha indovinato! Estratto: ${extracted}\n******`)
+    console.log(`******\n${p1.name} ha indovinato! \nEstratto: ${extracted}\n******`)
+    console.log(`scelta di ${p1.name}: ${p1.choice}`)
+    console.log(`scelta di ${p2.name}: ${p2.choice}`)
 }else if(p2.choice == extracted){
-    console.log(`******\n ${p2.name} ha indovinato! Estratto: ${extracted}\n******`)
-}else if(p1.error < p2.error){
-    console.log(`***\nNessuno ha indovinato, ma ${p1.name} si è avvicinato di più, bravoh \n Estratto: ${extracted} \n scelto: ${p1.choice}\n***`)
+    console.log(`******\n${p2.name} ha indovinato! \nEstratto: ${extracted}\n******`)
+    console.log(`scelta di ${p1.name}: ${p1.choice}`)
+    console.log(`scelta di ${p2.name}: ${p2.choice}`)
+}else if(p1.error(extracted) < p2.error(extracted)){
+    console.log(`***\nNessuno ha indovinato, ma ${p1.name} si è avvicinato di più, bravo! \nEstratto: ${extracted}`)
+    console.log(`scelta di ${p1.name}: ${p1.choice}`)
+    console.log(`scelta di ${p2.name}: ${p2.choice}`)
 }else{
-    console.log(`***\nNessuno ha indovinato, ma ${p2.name} si è avvicinato di più, bravoh \n Estratto: ${extracted} \n scelto: ${p2.choice}\n***`)
+    console.log(`***\nNessuno ha indovinato, ma ${p2.name} si è avvicinato di più, bravo! \nEstratto: ${extracted}`)
+    console.log(`scelta di ${p1.name}: ${p1.choice}`)
+    console.log(`scelta di ${p2.name}: ${p2.choice}`)
 }
 
 }
